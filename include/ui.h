55c921e49d052e2f21bca627b1e7eb3018f9abc4
PatDu621 <3076952150@qq.com>
2026-01-18 14:36:41 +0800

    Update ui.h

--- include/ui.h
+++ include/ui.h
@@ -1,14 +1,28 @@
-
 #pragma once
+#include "thirdparty/glm/glm.hpp"
 
 struct UIState {
-    float camera_speed = 3.0f;
-    float camera_fov = 60.0f;
-    bool load_model = false;
+    // ===== 1. 摄像机参数 =====
+    glm::vec3 camera_pos = glm::vec3(0.0f, 3.5f, 3.0f); 
+    float     camera_yaw = -45.0f;
+    float     camera_pitch = 20.0f;
+    float     camera_speed = 3.0f;
+    float     camera_fov = 60.0f;
 
-    bool mouse_left_down = false; 
-    float camera_yaw = -90.0f;  
-    float camera_pitch = 0.0f;
+    // ===== 2. 长方体创建参数=====
+    float     cube_width = 1.0f;
+    float     cube_height = 1.0f;
+    float     cube_depth = 1.0f;
+    glm::vec3 cube_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    // ===== 3. 光源位置参数=====
+    glm::vec3 light_pos = glm::vec3(2.0f, 3.0f, 5.0f);
+
+    // ===== 4. 模型位置参数 =====
+    glm::vec3 model_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    // ===== 辅助标记 =====
+    bool load_model = false;
 };
 
 void ui_draw(UIState& state);
